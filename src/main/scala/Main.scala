d570183e8d1b8b707ef073e46f4d3d5aeb7385ab
Colby Meier <colbymeier1337@gmail.com>
2017-06-28 18:21:44 -0600

    Transitioned to tuple math, permutations was hella heavy

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,15 +8,18 @@ object Main {
       case None => Array(0)
     }
     val (target: Int, count: Int) = (args.tail.head.toInt, args.tail.tail.head.toInt) // Make safe
-    println(s"calling subset with ${intList} ${target}")
-    println(subset(intList.toList, target, count))
+    val result = outerTuple(intList.toList, target, count)
+    println(s"Achieved Value: ${result.value} by adding ${result.parts(0)} and ${result.parts(1)}")
   }
 
 
 
-  def subset(list: List[Int], targetValue: Int, n: Int): List[List[Int]] = {
-    // VERY N(O) heavy on large numbers, would be best with a stream
-    val allCombos = list.permutations.toList.map(x => x.combinations(list.size / n))
-    val items: List[List[Int]: allCombos.flatMap(_.sliding(n).sum)
+  case class AnswerInfo(value: Int, parts: List[Int])
+  def outerTuple(list: List[Int], targetValue: Int, n: Int): AnswerInfo = {
+    val parts = list.sorted.reverse.zipWithIndex.partition(x => x._2 % 2 == 0)
+    val x: List[Int] = parts._1.map(x => x._1)
+    val y: List[Int] = parts._2.map(x => x._1)
+    val sum = x.mkString.toInt + y.mkString.toInt
+    AnswerInfo(value = sum, parts = List(x.mkString.toInt, y.mkString.toInt))
   }
 }
